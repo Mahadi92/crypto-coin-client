202e0cdf88d441865a5a9fcedd08082bfbe030f0
Mahadi92 <mahadi.hasan.937524.mh@gmail.com>
2022-04-07 00:23:10 +0600

    Environment ready

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
+import Script from "next/script"
+
+// All styles
+
+//global styles
+import 'bootstrap/dist/css/bootstrap.css';
+import '../styles/global/globals.css';
+import '../styles/global/global.scss';
+import '../styles/home.scss';
+import Navbar from "../components/global/Navbar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <>
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+        crossorigin="anonymous" />
+
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
+
