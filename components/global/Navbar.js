309e36cc7765ca32327936d2b5892e11c8de515f
Mahadi92 <mahadi.hasan.937524.mh@gmail.com>
2022-04-07 01:37:35 +0600

    navbar done

--- components/global/Navbar.js
+++ components/global/Navbar.js
@@ -1,38 +1,43 @@
 import Link from 'next/link';
 import Image from "next/image";
-import React from 'react';
+import navData from "../../data/navbarData.json";
+import { RiGlobalLine } from "react-icons/ri";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg c_container py-4">
       <div className="container-fluid">
         <Link href="/">
           <a className="navbar-brand">
             <Image src="/static/brand_logo.png" width="150" height="30" alt="Crypto Coin" />
           </a>
         </Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+        <button className="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon navbar-toggler-icon-custom"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Features</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Pricing</a>
-            </li>
+          <ul className="navbar-nav ms-auto">
+            {
+              navData.map((item, i) => {
+                return (
+                  <li key={i} className="nav-item">
+                    <Link href={item.link}>
+                      <a className="nav-link pe-4 pe-xl-5 text-dark">{item.linkText}</a>
+                    </Link>
+                  </li>
+                )
+              })
+            }
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown link
+                <RiGlobalLine size="25px" />
               </a>
               <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <li><a className="dropdown-item" href="#">Action</a></li>
-                <li><a className="dropdown-item" href="#">Another action</a></li>
-                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                <li>
+                  <Link href="dashboard">
+                    <a className="dropdown-item">Dashboard</a>
+                  </Link>
+                </li>
               </ul>
             </li>
           </ul>
