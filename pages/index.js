8eb4a0bb21f6ab4e2ad7579dcd5c4e9277e5da44
Mahadi92 <mahadi.hasan.937524.mh@gmail.com>
2022-04-09 00:12:42 +0600

    second section (what is Akacoin) done

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,42 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import Link from 'next/link';
+import whatIsAkacoinData from "../data/home/whatIsAkacoinData.json";
+import { BsArrowRight } from "react-icons/bs"
 
 export default function Home() {
-  return (
-    <main className="bg-primary c_container">
 
+  // Second section data
+  const { sectionHeading, subHeading, paragraph, link, linkText, img1, img2, videoLink } = whatIsAkacoinData;
+
+  return (
+    <main>
+      <header>
+
+      </header>
+
+      <section className="home__dual_div_container">
+        <h2 className="heading">{whatIsAkacoinData.sectionHeading}</h2>
+
+        <div className="home_dd__content_wrapper">
+          <div className="home_dd__subheading_wrapper">
+            <h3>{whatIsAkacoinData.subHeading}</h3>
+          </div>
+          <div className="home_dd__paragraph_wrapper">
+            <p>{whatIsAkacoinData.paragraph}</p>
+            <Link href={whatIsAkacoinData.link}>
+              <a className="home_ddp__link">{whatIsAkacoinData.linkText} <BsArrowRight size="25px" /></a>
+            </Link>
+          </div>
+        </div>
+        <div className="home_dd__img_wrapper">
+          <div className="home_ddi__img1">
+            <Image src={whatIsAkacoinData.img1} layout="responsive" width="80%" height="70" objectFit="content" alt="" />
+          </div>
+          <div className="home_ddi__img2">
+            <Image src={whatIsAkacoinData.img2} layout="responsive" width="100%" height="38" alt="" />
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
