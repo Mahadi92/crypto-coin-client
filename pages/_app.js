f53dcf9a515952bf184d77097b5070329f74f0f9
Mahadi92 <mahadi.hasan.937524.mh@gmail.com>
2022-04-12 00:40:20 +0600

    cleint site building done

--- pages/_app.js
+++ pages/_app.js
@@ -7,13 +7,13 @@ import Script from "next/script"
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/global/globals.css';
 import '../styles/global/global.scss';
-import '../styles/Footer.scss';
+import '../styles/footer.scss';
 //page styles
-import '../styles/Home.scss';
+import '../styles/home.scss';
 //components styles
-import "../styles/WalletSlider.scss";
-import "../styles/QuickGuideCard.scss";
-import "../styles/FacilitiesCard.scss";
+import "../styles/walletSlider.scss";
+import "../styles/quickGuideCard.scss";
+import "../styles/facilitiesCard.scss";
 
 import Navbar from "../components/global/Navbar";
 import Footer from "../components/global/Footer";
